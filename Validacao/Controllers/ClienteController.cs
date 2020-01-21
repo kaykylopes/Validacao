64664a0d45dd972e30253c1d747ac022eaadc2ef
kayky lopes <kaykylopes@hotmail.com>
2020-01-20 22:57:05 -0300

    Adicionar arquivos de projeto.

---
+++ Validacao/Controllers/ClienteController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Validacao.Models;
+
+namespace Validacao.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            var cliente = new Cliente();
+            return View(cliente);
+        }
+
+        [HttpPost]
+        public ActionResult Index( Cliente cliente)
+        {
+            //if (string.IsNullOrEmpty(cliente.Nome))
+            //    ModelState.AddModelError("Nome", "Nome é obrigatório");
+
+            //if (string.IsNullOrEmpty(cliente.Email))
+            //    ModelState.AddModelError("Email", "Campo Email é obrigatorio");
+
+            //if (string.IsNullOrEmpty(cliente.Telefone))
+            //    ModelState.AddModelError("Telefone", "Campo Telefone é obrigatorio");
+
+            //if (cliente.Idade == 0)
+            //{
+            //    ModelState.AddModelError("Idade","Idade é obrigatorio");
+            //}
+            //else
+            //{
+            //    if(cliente.Idade <= 21)
+            //    {
+            //        ModelState.AddModelError("Idade", "Idade informado deve ser maior que 21 anos");
+            //    }
+            //}
+
+
+            if (!ModelState.IsValid)
+            {
+                return View(cliente);
+            }
+            else
+            {
+                return View("Validacao", cliente);
+            }
+        }
+
+
+        public ActionResult Validacao(Cliente cliente)
+        {
+            return View(cliente);
+        }
+    }
+}
---
+++ Validacao/Models/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Validacao.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage =  "o Nome deve ser Informado")]
+        [StringLength(50, MinimumLength =5,ErrorMessage = "o Nome deve ter no minimo 5 caractere e maximo 50")]
+        [Display(Name ="Informe o nome do cliente")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "o Email deve ser Informado")]
+        [EmailAddress(ErrorMessage = "Informe o Email válido")]
+        public string Email { get; set; }
+
+        [StringLength(10, MinimumLength = 6, ErrorMessage = "o Telefone deve ter no minimo 6 caractere e maximo 10")]
+        public string Telefone { get; set; }
+
+        [Range(21,99, ErrorMessage ="A idade deve ser maior que 21 e menor que 99")]
+        public int Idade { get; set; }
+    }
+}
